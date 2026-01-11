cb4f09471f9128ac93747302fa6cd64166140306
qpasahumano <qpasahumano@gmail.com>
2026-01-11 11:23:23 -0300

    Create app.js

---
+++ humanometreo-v2/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("startTestBtn");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Inicio test semanal – HUMANÓMETRO v.2");
+    });
+  }
+});
